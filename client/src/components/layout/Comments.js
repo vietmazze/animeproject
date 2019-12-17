6bc45727940b9adbbb692eae377e0b7390ab47aa
vietmazze <dqnguyen2011@gmail.com>
2019-12-17 10:26:27 -0500

    comments box

--- client/src/components/layout/Comments.js
+++ client/src/components/layout/Comments.js
@@ -2,7 +2,29 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-const Comments = () => {
+const Comments = ({ comments }) => {
+  const [commentLoading, setCommentLoading] = useState(true);
+  const [replyLoading, setReplyLoading] = useState(false);
+
+  const onReplyClick = () => {
+    setReplyLoading(!replyLoading);
+  };
+  let reply;
+
+  if (replyLoading === true) {
+    reply = (
+      <div className='w-11/12 m-4'>
+        <input
+          type='text'
+          className='h-24 w-full'
+          placeholder='Join the discussion...'
+        />
+      </div>
+    );
+  } else {
+    console.log("empty");
+  }
+
   return (
     <div className='flex justify-start '>
       <div className='w-full lg:w-9/12 '>
@@ -28,6 +50,34 @@ const Comments = () => {
         <button className='bg-blue-500 text-white py-2 px-4 flex  ml-auto m-4'>
           Submit
         </button>
+        <div>
+          {!commentLoading ? (
+            <div> Be the first to comment ... </div>
+          ) : (
+            <div>
+              <div className='flex flex-row'>
+                <div className='lg:w-5 m-4 '>
+                  <span className='fa-layers fa-fw '>
+                    <FontAwesomeIcon
+                      icon={faUser}
+                      size='3x'
+                      color='white'
+                      transform='down-4'
+                    />
+                  </span>
+                </div>
+                <div className='w-11/12 m-4'>
+                  <div className='flex flex-col'>
+                    <h3> Username</h3>
+                    <p>Some comment</p>
+                  </div>
+                  <button onClick={onReplyClick}>Reply</button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+        {reply}
       </div>
     </div>
   );
--- client/src/stories/13-Comments.stories.js
+++ client/src/stories/13-Comments.stories.js
@@ -2,4 +2,14 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Comments from "../components/layout/Comments";
 
-storiesOf("CommentBox", module).add("default", () => <Comments />);
+const comments = [
+  {
+    user: "Winzkay",
+    comment:
+      "Despite me hating recap episodes, this one was well put together and I needed it to be honest 😅 I want a meat bun"
+  }
+];
+
+storiesOf("CommentBox", module).add("default", () => (
+  <Comments comments={comments} />
+));
